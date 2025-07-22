c87880b434ad13044f2bfb6b15e9d8253fb369cc
ganislp@gmail.com <ganislp@gmail.com>
2025-07-22 11:24:12 +0200

    add beanstalk

--- infrastructure/modules/aws-_beanstalk/variables.tf
+++ infrastructure/modules/aws-_beanstalk/variables.tf
@@ -7,3 +7,27 @@ variable "project_name" {
   type        = string
   description = "Project name"
 }
+
+variable "vpc_id" {
+  type = string
+  description = "VPC ID"
+}
+
+variable "private_subnet_ids" {
+  type = list(string)
+  description = "private subnet ids"
+}
+variable "security_group_id" {
+  type = string
+  description = "security group id"
+}
+
+variable "lb_security_group_id" {
+  type = string
+  description = "security group id"
+}
+
+variable "key_name" {
+  type = string
+  description = "Key Name"
+}
